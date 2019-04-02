cb0f53b4d5c5c48e40ae682edffc13dc73089a0e
Awais Memon <awaismemon95@yahoo.com>
2019-04-02 15:53:24 +0500

    TwitterTextProcessing Code

---
+++ TwitterStreamAPI/config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "oJZxSkYAsqyDOop0ZDGunzDI5"
+consumer_secret = "xNY2KpqTGT2hJCCG9GuyFToR8kbFItQk6JXKPkVCg2JTLK5TPa"
+access_key = "1092905297881022464-YOOVRQwx5FgMZYnK6pEdVGfEn4fXJD"
+access_secret = "IiEiOGQ0f4DA3yvDBLQ92AtIACxdczHjRIbAFbaLsobZn"
